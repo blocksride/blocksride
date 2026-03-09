c2035d37aaff64cf6a4d73626d091e340f2db278
Allan Robinson <allanrobinson@Allans-MacBook-Pro.local>
2026-03-09 17:36:01 +0300

    Update demo-related landing and route wiring

--- client/src/router/routes.tsx
+++ client/src/router/routes.tsx
@@ -2,11 +2,13 @@ import { Routes, Route } from 'react-router-dom'
 import { Landing } from '../pages/Landing'
 import { Terminal } from '../pages/Terminal'
 import { Terms } from '../pages/Terms'
+import { Demo } from '../pages/Demo'
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Landing />} />
+      <Route path="/demo" element={<Demo />} />
       <Route path="/terminal" element={<Terminal />} />
       <Route path="/terms" element={<Terms />} />
     </Routes>
